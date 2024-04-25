b00ccaee76e66b27cb269c751871e6d95c1449cc
Mantas Jakstas <mantasjob13@gmail.com>
2024-04-25 18:24:48 +0100

    Initial Commit

---
+++ DisneyApp/DisneyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisneyAppApp.swift
+//  DisneyApp
+//
+//  Created by Mantas Jakstas on 25/4/24.
+//
+
+import SwiftUI
+
+@main
+struct DisneyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
